9d4a0fcf0bf54f2f76c9a47bd4ba66f6f80b2b5a
shimadakento <kentskind@gmail.com>
2018-08-18 14:52:25 +0900

    modify addAlbumInfoToDB

--- addAlbumInfoToDB.py
+++ addAlbumInfoToDB.py
@@ -1,10 +1,12 @@
 import sqlite3
+from typing import Dict, List, Any
+from urllib.parse import urlparse, parse_qs
 from createSpotifyConnection import create_spotify_connection
 
 
 def add_albuminfo_to_db():
 
-    db_filename = 'DreamTheater.db'
+    db_filename = 'Spotify_PreCure.db'
     conn = sqlite3.connect(db_filename)
     c = conn.cursor()
 
@@ -14,17 +16,45 @@ def add_albuminfo_to_db():
 
     spotify = create_spotify_connection()
 
-    albumdata = spotify.artist_albums(artist_id='2aaLAng2L2aWD2FClzwiep', limit=50, album_type='album')
+    nextURL: object = 'initial'
+    albumcount = 0
 
-    print(len(albumdata['items']))
+    while nextURL is not None:
+        isInitial = True if nextURL == 'initial' else False
+        next_qs = None if isInitial else parse_qs(urlparse(nextURL).query)
+        offset = 0 if isInitial else next_qs['offset'][0]
+        limit = 20 if isInitial else next_qs['limit'][0]
 
-    sql = 'insert into ALBUMDETAILS (SPOTIFY_ID, NAME, RELEASE_DATE) values (?,?,?);'
+        results = spotify.search(q='プリキュア',
+                                 type='album',
+                                 offset=offset,
+                                 limit=limit)
 
-    for album in albumdata['items']:
-        data = (album['id'], album['name'], album['release_date'])
-        c.execute(sql, data)
+        for album in results['albums']['items']:
+            # albumDetail = spotify.album(album["id"])
+            print(album["name"])
+            add_or_dont = input('Add {} to DB?[Y/n]'.format(album['name']))
+            if add_or_dont in ['n', 'N', 'no', 'NO', 'No']:
+                continue
+            else:
+                data = (album['id'],
+                        album['artists'][0]['id'],
+                        album['name'],
+                        album['external_urls']['spotify'],
+                        album['images'][1]['url'],
+                        album['release_date'],
+                        album['release_date_precision'],
+                        album['total_tracks'])
+                c.execute(sql, data)
+                albumcount+=1
+
+        if results['albums']['next']:
+            nextURL = results['albums']['next']
+        else:
+            nextURL = None
 
     conn.commit()
+    print('end')
 
 
 if __name__ == '__main__':
