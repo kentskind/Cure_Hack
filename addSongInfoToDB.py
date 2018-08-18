a66a1a4b4d71c2fb0147c8032fadcb2f2d545afc
shimadakento <kentskind@gmail.com>
2018-08-18 15:09:20 +0900

    modify addSongInfoToDB

--- addSongInfoToDB.py
+++ addSongInfoToDB.py
@@ -4,20 +4,29 @@ from createSpotifyConnection import create_spotify_connection
 
 def add_songinfo_to_db():
 
-    db_filename = 'DreamTheater.db'
+    db_filename = 'Spotify_PreCure.db'
     conn = sqlite3.connect(db_filename)
     c = conn.cursor()
 
     spotify = create_spotify_connection()
 
-    albumIDs = c.execute('select SPOTIFY_ID, NAME from ALBUMDETAILS;')
-    sql = 'insert into SONGDETAILS (SPOTIFY_ID, ALBUM_ID, NAME, DURATION_MS, TRACK_NUMBER) values (?,?,?,?,?)'
+    albumIDs = c.execute('select album_key, name from albums;')
+    sql = 'insert into songs ' \
+          '(song_key, artist_key, album_key, title, href_url, duration_ms, disk_number, track_number) ' \
+          'values (?,?,?,?,?,?,?,?)'
 
     for album in albumIDs.fetchall():
         print('Processing {}...'.format(album[1]))
         songs = spotify.album_tracks(album_id=album[0], limit=50)
         for song in songs['items']:
-            songdata = (song['id'], album[0], song['name'], int(song['duration_ms']), int(song['track_number']))
+            songdata = (song['id'],
+                        song['artists'][0]['id'],
+                        album[0], song['name'],
+                        song['external_urls']['spotify'],
+                        song['duration_ms'],
+                        song['disc_number'],
+                        song['track_number'])
+
             print(songdata)
             c.execute(sql, songdata)
 
